1ef42a07920eba693b19e6fbb21da0224de437b3
rahulkhatri137 <rahulkhatri137@gmail.com>
2022-05-27 08:10:00 +0530

    Minor Fixes & Action WF
    Signed-off-by: rahulkhatri137 <rahulkhatri137@gmail.com>

--- make/add_phh/system/bin/rw-system.sh
+++ make/add_phh/system/bin/rw-system.sh
@@ -942,6 +942,10 @@ if getprop ro.vendor.build.fingerprint |grep -qiE '^samsung/';then
     setprop persist.sys.phh.fod.samsung true
 fi
 
+if getprop ro.vendor.build.fingerprint | grep -q -e samsung/o1s -e samsung/t2s -e samsung/p3s; then
+    setprop persist.sys.phh.ultrasonic_udfps true
+fi
+
 if getprop ro.vendor.build.fingerprint |grep -qiE -e ASUS_I006D -e ASUS_I003;then
 	setprop persist.sys.phh.fod.asus true
 fi
@@ -1093,3 +1097,71 @@ fi
 
 mount -o bind /mnt/phh/empty_dir /vendor/app/qti-logkit
 mount -o bind /mnt/phh/empty_dir /vendor/app/qti-logkit-lite
+
+
+# Display CutOut
+mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationTall || true
+mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationDouble || true
+mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationCorner || true
+
+# Drop btconfigstore and weaver from manifest
+for f in \
+    /vendor/etc/vintf/manifest.xml \
+    /vendor/manifest.xml; do # For O if i ever wanted to try
+    [ ! -f "$f" ] && continue
+    if grep -q -E "vendor.qti.hardware.btconfigstore|android.hardware.weaver" "$f"; then
+        # shellcheck disable=SC2010
+        ctxt="$(ls -lZ "$f" | grep -oE 'u:object_r:[^:]*:s0')"
+        b="$(echo "$f" | tr / _)"
+        cp -a "$f" "/mnt/phh/$b"
+        sed -i "s|vendor.qti.hardware.btconfigstore|vendor.qti.hardware.btconfigstore_disable|g" "/mnt/phh/$b"
+        sed -i "s|android.hardware.weaver|android.hardware.weaver_disable|g" "/mnt/phh/$b"
+        chcon "$ctxt" "/mnt/phh/$b"
+        mount -o bind "/mnt/phh/$b" "$f"
+    fi
+done
+
+# lib binds
+mount -o bind /system/lib/vndk-"$vndk"/libgui.so /vendor/lib/libgui_vendor.so || true
+mount -o bind /system/lib64/vndk-"$vndk"/libgui.so /vendor/lib64/libgui_vendor.so || true
+mount -o bind /system/lib/vndk-"$vndk"/libbinder.so /vendor/lib/libbinder.so || true
+mount -o bind /system/lib64/vndk-"$vndk"/libbinder.so /vendor/lib64/libbinder.so || true
+mount -o bind /system/lib/vndk-"$vndk"/libbinder.so /vendor/lib/vndk/libbinder.so || true
+mount -o bind /system/lib64/vndk-"$vndk"/libbinder.so /vendor/lib64/vndk/libbinder.so || true
+mount -o bind /system/lib/vndk-sp-"$vndk"/libcutils.so /vendor/lib/libcutils.so || true
+mount -o bind /system/lib64/vndk-sp-"$vndk"/libcutils.so /vendor/lib64/libcutils.so || true
+mount -o bind /system/lib/vndk-sp-"$vndk"/libcutils.so /vendor/lib/vndk-sp/libcutils.so || true
+mount -o bind /system/lib64/vndk-sp-"$vndk"/libcutils.so /vendor/lib64/vndk-sp/libcutils.so || true
+mount -o bind /system/lib/libpdx_default_transport.so /vendor/lib/libpdx_default_transport.so || true
+mount -o bind /system/lib64/libpdx_default_transport.so /vendor/lib64/libpdx_default_transport.so || true
+mount -o bind /system/lib/libpdx_default_transport.so /vendor/lib/vndk/libpdx_default_transport.so || true
+mount -o bind /system/lib64/libpdx_default_transport.so /vendor/lib64/vndk/libpdx_default_transport.so || true
+
+# Drop qcom stuffs for non qcom devices
+if ! getprop ro.hardware | grep -qiE -e qcom -e mata;then
+    mount -o bind /mnt/phh/empty_dir /system/app/imssettings || true
+    mount -o bind /mnt/phh/empty_dir /system/priv-app/ims || true
+    mount -o bind /mnt/phh/empty_dir /system/app/ims || true
+    mount -o bind /mnt/phh/empty_dir /system/app/QtiTelephonyService || true
+    mount -o bind /mnt/phh/empty_dir /system/app/datastatusnotification || true
+fi
+
+# Fix no Earpiece in audio_policy
+for f in \
+    /odm/etc/audio_policy_configuration.xml \
+    /vendor/etc/audio_policy_configuration.xml; do
+    [ ! -f "$f" ] && continue
+    if ! grep -q "<item>Earpiece</item>" "$f"; then
+        # shellcheck disable=SC2010
+        ctxt="$(ls -lZ "$f" | grep -oE 'u:object_r:[^:]*:s0')"
+        b="$(echo "$f" | tr / _)"
+        cp -a "$f" "/mnt/phh/$b"
+        sed -i "s|<attachedDevices>|<attachedDevices><item>Earpiece</item>|g" "/mnt/phh/$b"
+        chcon "$ctxt" "/mnt/phh/$b"
+        mount -o bind "/mnt/phh/$b" "$f"
+    fi
+done
+
+frp_node="$(getprop ro.frp.pst)"
+chown -h system.system $frp_node
+chmod 0660 $frp_node
