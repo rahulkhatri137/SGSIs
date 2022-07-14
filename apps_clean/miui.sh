20f6f1139377b23f1927111ccd8009c4b1527d9d
rahulkhatri137 <rahulkhatri137@gmail.com>
2022-07-14 07:06:36 +0000

    Changes: Build info,Debloat,Overlays
    Signed-off-by: rahulkhatri137 <rahulkhatri137@gmail.com>

--- apps_clean/miui.sh
+++ apps_clean/miui.sh
@@ -30,29 +30,20 @@ rm -rf $1/product/media/audio/notifications/*
 rm -rf $1/product/media/audio/ringtones/*
 rm -rf $1/*app/*eibo*
 # Nuke MIUI useless system apks
-rm -rf $1/app/cit
 rm -rf $1/app/MiuiCompass
 rm -rf $1/app/MiuiVideoGlobal
 rm -rf $1/app/PaymentService
 rm -rf $1/app/Calculator
 rm -rf $1/app/VoiceAssist
-rm -rf $1/app/MiRadio
-rm -rf $1/app/Mipay
 rm -rf $1/app/NextPay
 rm -rf $1/app/com.miui.qr
 rm -rf $1/app/MiuiBugReport
 rm -rf $1/app/AnalyticsCore
 rm -rf $1/app/BasicDreams
 rm -rf $1/app/BugReport
-rm -rf $1/app/Drive
-rm -rf $1/app/Duo
-rm -rf $1/app/Gmail2
 rm -rf $1/app/HybridAccessory
 rm -rf $1/app/HybridPlatform
-rm -rf $1/app/InMipay
 rm -rf $1/app/MSA-Global
-rm -rf $1/app/Maps
-rm -rf $1/app/MiPicks
 rm -rf $1/app/MiuiAudioMonitor
 rm -rf $1/app/MiuiVideoPlayer
 rm -rf $1/app/Music2
@@ -60,9 +51,7 @@ rm -rf $1/app/Photos
 rm -rf $1/app/TranslationService
 rm -rf $1/app/Videos
 rm -rf $1/app/YouDaoEngine
-rm -rf $1/app/YouTube
 rm -rf $1/app/facebook-appmanager
-rm -rf $1/app/talkback
 rm -rf $1/app/Lens
 rm -rf $1/app/CatchLog
 rm -rf $1/app/Joyose
@@ -118,7 +107,6 @@ rm -rf $1/priv-app/Backup
 rm -rf $1/priv-app/Browser
 rm -rf $1/priv-app/Calendar
 rm -rf $1/priv-app/MiuiScanner
-rm -rf $1/priv-app/Velvet
 rm -rf $1/priv-app/Weather
 rm -rf $1/priv-app/WeatherProvider
 rm -rf $1/priv-app/MiuiVideo
@@ -133,16 +121,12 @@ rm -rf $1/priv-app/MiDrop
 rm -rf $1/priv-app/MiuiBrowserGlobal
 rm -rf $1/priv-app/PersonalAssistantGlobal
 rm -rf $1/priv-app/SoundRecorder
-rm -rf $1/priv-app/Turbo
 rm -rf $1/priv-app/YellowPage
 rm -rf $1/priv-app/facebook-installer
 rm -rf $1/priv-app/facebook-services
-rm -rf $1/priv-app/MiShare
-rm -rf $1/priv-app/Tag
 rm -rf $1/priv-app/MiuiMusicGlobal
 rm -rf $1/priv-app/MiCalendarGlobal
 rm -rf $1/priv-app/MiBrowserGlobal
-rm -rf $1/priv-app/AuthManager
 rm -rf $1/priv-app/MiuiExtraPhoto
 rm -rf $1/priv-app/ONS
 rm -rf $1/priv-app/StatementService
@@ -150,38 +134,13 @@ rm -rf $1/priv-app/CtsShimPrivPrebuilt
 rm -rf $1/priv-app/LocalTransport
 rm -rf $1/priv-app/MiMover
 rm -rf $1/priv-app/QuickSearchBox
-rm -rf $1/priv-app/MiRecycle
 rm -rf $1/priv-app/CleanMaster
 rm -rf $1/priv-app/FusedLocation
-rm -rf $1/priv-app/MiuiAod
 rm -rf $1/priv-app/MiGameCenterSDKService
 rm -rf $1/priv-app/MiRcs
 rm -rf $1/priv-app/NewHome
 
-# Nuke MIUI useless product apks
-rm -rf $1/product/app/Gmail2
-rm -rf $1/product/app/GoogleTTS
-rm -rf $1/product/app/Maps
-rm -rf $1/product/app/YouTube
-rm -rf $1/product/app/talkback
-rm -rf $1/product/app/ModuleMetadataGoogle
-rm -rf $1/product/app/uceShimService
-rm -rf $1/product/app/CalendarGoogle
-rm -rf $1/product/app/Chrome
-rm -rf $1/product/app/ModuleMetadataGooglePrebuilt
-rm -rf $1/product/app/GoogleOne
-rm -rf $1/product/priv-app/Velvet
-rm -rf $1/product/priv-app/CarrierServices
-rm -rf $1/product/priv-app/AndroidAutoStub
-rm -rf $1/product/priv-app/HotwordEnrollmentOKGoogleHEXAGON
-rm -rf $1/product/priv-app/Turbo
-rm -rf $1/product/priv-app/HotwordEnrollmentXGoogleHEXAGON
-rm -rf $1/product/priv-app/GoogleAssistant
-rm -rf $1/product/priv-app/GoogleFeedback
-rm -rf $1/product/priv-app/QAS_DVC_MSP
-
-
-app_list="InMIpay MIDrop MiLinkGlobal miPlayClient MIUIFileExplorer MIUICompass Cit MIUIHealthGlobal MiuiScanner MIUIMiPicks MIUINotes MIWallpaper MIUITouchAssistantGlobal XMSFKeepekAll DynamicSystemInstallationService MIService MIShare* MiuiCamera MIUIMusic* MIUIYellowPage* DeviceInfo DynamicDDSService dpmserviceapp EmergencyInfo GoogleFeedback seccamservice WfdService"
+app_list="InMIpay MIDrop MiLinkGlobal miPlayClient MIUIFileExplorer MIUICompass Cit MIUIHealthGlobal MiuiScanner MIUIMiPicks MIUINotes MIWallpaper MIUITouchAssistantGlobal XMSFKeepekAll DynamicSystemInstallationService MIShare* MiuiCamera MIUIMusic* MIUIYellowPage* DeviceInfo DynamicDDSService dpmserviceapp EmergencyInfo GoogleFeedback seccamservice WfdService MiuiAod MiRecycle MiPics MiPay MiRadio"
 for app in $app_list; do
 rm -rf $1/*app/$app
 rm -rf $1/*/*app/$app
