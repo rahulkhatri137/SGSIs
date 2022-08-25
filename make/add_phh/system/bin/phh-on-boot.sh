51d499f29ef8c0d08519f1af3f555062dd00ceb3
rahulkhatri137 <rahulkhatri137@gmail.com>
2022-08-25 15:06:46 +0000

    SGSI: Small Fixes
    Signed-off-by: rahulkhatri137 <rahulkhatri137@gmail.com>

--- make/add_phh/system/bin/phh-on-boot.sh
+++ make/add_phh/system/bin/phh-on-boot.sh
@@ -38,10 +38,6 @@ sleep 10
 
 minijailSrc=/system/system_ext/apex/com.android.vndk.v28/lib/libminijail.so
 minijailSrc64=/system/system_ext/apex/com.android.vndk.v28/lib64/libminijail.so
-if [ "$vndk" = 27 ];then
-    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
-    mount $minijailSrc /vendor/lib/libminijail_vendor.so
-fi
 
 if [ "$vndk" = 28 ];then
     mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
--- make/add_phh/system/bin/rw-system.sh
+++ make/add_phh/system/bin/rw-system.sh
@@ -789,7 +789,7 @@ for abi in "" 64;do
     f=/vendor/lib$abi/libstagefright_foundation.so
     if [ -f "$f" ];then
         for vndk in 26 27 28 29;do
-            mount "$f" /system/system_ext/apex/com.android.vndk.v$vndk/lib$abi/libstagefright_foundation.so
+            mount "$f" /system/apex/com.android.vndk.v$vndk/lib$abi/libstagefright_foundation.so
         done
     fi
 done
@@ -1114,23 +1114,6 @@ mount -o bind /mnt/phh/empty_dir /vendor/app/qti-logkit-lite
 mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationTall || true
 mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationDouble || true
 mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationCorner || true
-
-# Drop btconfigstore and weaver from manifest
-for f in \
-    /vendor/etc/vintf/manifest.xml \
-    /vendor/manifest.xml; do # For O if i ever wanted to try
-    [ ! -f "$f" ] && continue
-    if grep -q -E "vendor.qti.hardware.btconfigstore|android.hardware.weaver" "$f"; then
-        # shellcheck disable=SC2010
-        ctxt="$(ls -lZ "$f" | grep -oE 'u:object_r:[^:]*:s0')"
-        b="$(echo "$f" | tr / _)"
-        cp -a "$f" "/mnt/phh/$b"
-        sed -i "s|vendor.qti.hardware.btconfigstore|vendor.qti.hardware.btconfigstore_disable|g" "/mnt/phh/$b"
-        sed -i "s|android.hardware.weaver|android.hardware.weaver_disable|g" "/mnt/phh/$b"
-        chcon "$ctxt" "/mnt/phh/$b"
-        mount -o bind "/mnt/phh/$b" "$f"
-    fi
-done
 
 # lib binds
 mount -o bind /system/lib/vndk-"$vndk"/libgui.so /vendor/lib/libgui_vendor.so || true
